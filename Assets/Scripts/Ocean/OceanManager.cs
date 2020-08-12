4acb30ced65b68d70ae1ec46422ee3310e5773cb
Paul Mieschke <p.mieschke@gmail.com>
2020-08-13 01:17:07 +0200

    Floating physics and first ship added

---
+++ Assets/Scripts/Ocean/OceanManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Crest;
+
+namespace Ocean {
+    public class OceanManager : MonoBehaviour {
+        public static OceanManager Inst { get; private set; }
+
+        private void Awake() {
+            Inst = this;
+        }
+
+        /// <summary>
+        /// Get the water height at the given world position
+        /// </summary>
+        /// <param name="worldPos">3D world position</param>
+        /// <param name="minWaveLength">Optional: ignore wave lengths below this value; with ships use the ship width for this value</param>
+        /// <returns>The height of the ocean at the given world position</returns>
+        public static float SampleWaterHeight(Vector3 worldPos, float minWaveLength = 0f) {
+            float height = 0f;
+
+            SampleHeightHelper sampler = new SampleHeightHelper();
+            sampler.Init(worldPos, minWaveLength);
+            sampler.Sample(ref height);
+
+            return height;
+        }
+
+        /// <summary>
+        /// Get the water surface flow at the given world position
+        /// </summary>
+        /// <param name="worldPos">3D world position</param>
+        /// <param name="minWaveLength">Optional: ignore wave lengths below this value; with ships use the ship width for this value</param>
+        /// <returns>The surface flow of the ocean at the given world position</returns>
+        public static Vector2 SampleWaterFlow(Vector3 worldPos, float minWaveLength = 0f) {
+            Vector2 flow = Vector3.zero;
+
+            SampleFlowHelper sampler = new SampleFlowHelper();
+            sampler.Init(worldPos, minWaveLength);
+            sampler.Sample(ref flow);
+
+            return flow;
+        }
+    }
+}
---
+++ Assets/Scripts/Ocean/OceanPhysics/FloatPhysics.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Crest;
+
+namespace Ocean.OceanPhysics {
+    [RequireComponent(typeof(Rigidbody))]
+    public class FloatPhysics : MonoBehaviour {
+        private const float WATER_DENSITY = 1000f;
+        private readonly float WATER_BUOYANCY = WATER_DENSITY * Mathf.Abs(Physics.gravity.y);
+
+        [Range(0f, 2f)]
+        public float gizmoSize = 1f;
+
+        public Vector3 centerOfMass;
+        public List<FloatPoint> floatPoints = new List<FloatPoint>();
+        public float forceFactor = 1f;
+        /// <summary>
+        /// Ignore wave lengths below this value, usually the boat width
+        /// </summary>
+        [Tooltip("Ingore wave lengths below this value, usually the boat width")]
+        public float minWaveLength = 0f;
+
+        private Rigidbody rb;
+        private float totalFloatPointsWeight;
+
+        private Vector3[] samplePoints;
+        private Vector3[] sampleResDisplacements;
+        private Vector3[] sampleResVelocities;
+
+        private void Awake() {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Start() {
+            rb.centerOfMass = centerOfMass;
+            CalcTotalFloatPointsWeigth();
+
+            samplePoints = new Vector3[floatPoints.Count + 1];
+            sampleResDisplacements = new Vector3[floatPoints.Count + 1];
+            sampleResVelocities = new Vector3[floatPoints.Count + 1];
+        }
+
+        private void FixedUpdate() {
+#if UNITY_EDITOR
+            CalcTotalFloatPointsWeigth();
+#endif
+
+            UpdateWaterSamples();
+
+            ApplyBuoyancy();
+            ApplyDrag();
+        }
+
+        private void CalcTotalFloatPointsWeigth() {
+            totalFloatPointsWeight = 0f;
+            floatPoints.ForEach(fp => totalFloatPointsWeight += fp.weight);
+        }
+
+        private void UpdateWaterSamples() {
+            for (int i = 0; i < floatPoints.Count; i++) {
+                samplePoints[i] = transform.TransformPoint(floatPoints[i].offsetPos + Vector3.up * centerOfMass.y);
+            }
+            samplePoints[floatPoints.Count] = transform.position;
+
+            OceanRenderer.Instance.CollisionProvider.Query(GetHashCode(), minWaveLength, samplePoints, sampleResDisplacements, null, sampleResVelocities);
+        }
+
+        private void ApplyBuoyancy() {
+            for (int i = 0; i < floatPoints.Count; i++) {
+                float waterHeight = OceanRenderer.Instance.SeaLevel + sampleResDisplacements[i].y;
+                float heightDiff = waterHeight - samplePoints[i].y;
+                if (heightDiff > 0) {
+                    // Below water surface -> apply water buoyancy force
+                    rb.AddForceAtPosition(WATER_BUOYANCY * heightDiff * Vector3.up * floatPoints[i].weight * forceFactor / totalFloatPointsWeight, samplePoints[i]);
+                }
+            }
+        }
+
+        private void ApplyDrag() {
+            Vector3 waterSurfaceVelocity = sampleResVelocities[floatPoints.Count];
+            Vector2 surfaceFlow = OceanManager.SampleWaterFlow(transform.position, minWaveLength);
+            waterSurfaceVelocity += new Vector3(surfaceFlow.x, 0, surfaceFlow.y);
+
+            // Apply drag relative to water
+            Vector3 velocityRelativeToWater = rb.velocity - waterSurfaceVelocity;
+
+            Vector3 forcePosition = rb.position;
+            rb.AddForceAtPosition(Vector3.up * Vector3.Dot(Vector3.up, -velocityRelativeToWater), forcePosition, ForceMode.Acceleration);
+            rb.AddForceAtPosition(transform.right * Vector3.Dot(transform.right, -velocityRelativeToWater), forcePosition, ForceMode.Acceleration);
+            rb.AddForceAtPosition(transform.forward * Vector3.Dot(transform.forward, -velocityRelativeToWater), forcePosition, ForceMode.Acceleration);
+        }
+
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawCube(transform.TransformPoint(centerOfMass), Vector3.one * gizmoSize);
+
+            Gizmos.color = Color.cyan;
+            foreach (FloatPoint fp in floatPoints) Gizmos.DrawSphere(transform.TransformPoint(fp.offsetPos + new Vector3(0, centerOfMass.y, 0)), gizmoSize * (0.1f + fp.weight));
+        }
+
+        [System.Serializable]
+        public class FloatPoint {
+            /// <summary>
+            /// Offset position from the parent gameobject
+            /// </summary>
+            public Vector3 offsetPos;
+
+            public float weight = 1f;
+        }
+    }
+}
